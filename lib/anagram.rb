c6c40a7af2b5b5b83e7bd6ac1884b3d582c5d7b6
Michael Kang <michaelkang85@gmail.com>
2018-02-10 22:46:21 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -14,7 +14,7 @@ class Anagram
       result << ele.split("").sort.join("")
   end
   result.each do |ele2|
-    result2 << self.word if ele2 == self.word.split("").sort.join("")
+    result2 << ele2 if ele2 == self.word.split("").sort.join("")
   end
   result2
 end
