bfee32f90ad4f2304a39887bb33f24eeadad4c20
Michael Kang <michaelkang85@gmail.com>
2018-02-10 22:43:06 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -13,7 +13,7 @@ class Anagram
       result << ele.split("").sort.join("")
   end
   result.each do |ele2|
-    result2 << ele2 if ele2 == self.word
+    result2 << ele2 if ele2 == self.word.sort
   end
   result2
 end
