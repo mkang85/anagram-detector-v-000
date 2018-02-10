f9263237e71b5ad18e631335e9c2d31655783994
Michael Kang <michaelkang85@gmail.com>
2018-02-10 22:38:03 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,17 @@
 # Your code goes here!
 class Anagram
-  attr_accessor :word
-  def initialize(word)
-    @word = word
-  end
+#   attr_accessor :word
+#
+#   def initialize(word)
+#     @word = word
+#   end
+# #
+#   def match(array)
+#     result = []
+#     array.map do |ele|
+#       ele.split("").sort
+#   end.each {|ele2| result << if ele2 == word}
+#   result
+# end
 
-  def match(array)
-    result = []
-    array.map do |ele|
-      ele.split("").sort
-  end.each {|ele2| result << if ele2 == word}
-  result
-end
 end
