22c9088415157114be7e8585d1614a3a4748097d
Michael Kang <michaelkang85@gmail.com>
2018-02-10 22:38:23 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,10 +1,10 @@
 # Your code goes here!
 class Anagram
-#   attr_accessor :word
-#
-#   def initialize(word)
-#     @word = word
-#   end
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
 # #
 #   def match(array)
 #     result = []
